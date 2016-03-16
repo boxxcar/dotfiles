f9549c915662c0bafacab5b64fadf32a31b0eade
boxxcar <swimfreealto@gmail.com>
2016-03-16 00:32:32 -0400

    Removed mediaplayer block. Added background feh script to i3config

--- Scripts/lock.sh
+++ Scripts/lock.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Credit goes to /u/wontonspecial (Github: savoca) and /u/fbt2lurker
-
 icon="$HOME/Pictures/Wallpapers/Icons/Lock3.png"
 
 # Check monitors and apply
-if (xrandr -q | grep "HDMI2 connected" && xrandr -q | grep "DP1 connected")
+if (xrandr -q | grep -q "HDMI2 connected" && xrandr -q | grep -q "DP1 connected")
 then
 	image="$HOME/Pictures/Wallpapers/MultiLock.png"
 else
