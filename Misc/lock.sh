f8cc695199bb5695a9b81fb71814b89d204ddb08
boxxcar <swimfreealto@gmail.com>
2016-03-16 18:51:52 -0400

    Changed lock.sh

--- Misc/lock.sh
+++ Misc/lock.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 icon="$HOME/Pictures/Wallpapers/Icons/Lock3.png"
-
-# Check monitors and apply
-if (xrandr -q | grep -q "HDMI2 connected" && xrandr -q | grep -q "DP1 connected")
-then
-	image="$HOME/Pictures/Wallpapers/MultiLock.png"
-else
-	image="$HOME/Pictures/Wallpapers/Lock.png"
-fi
+image="$HOME/Pictures/Wallpapers/Lock.png"
 
 (( $# )) && { icon=$1; }
 
